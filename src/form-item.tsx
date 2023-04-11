9c16326b84908a1ed300fad1b0c20aed83a9ea7c
Sam Noedel <sam.noedel@gmail.com>
2023-04-10 17:38:19 -0700

    Pass props through to inputs via form items

--- src/form-item.tsx
+++ src/form-item.tsx
@@ -1,25 +1,27 @@
-import { Validator } from './validation.js'
-import Submit from './submit.js'
-import Input from './input.js'
+import { Validator } from "./validation.js";
+import Submit from "./submit.js";
+import Input from "./input.js";
 
 interface Field<T extends string> {
-  label: string
-  type: T
+  label: string;
+  type: T;
 }
 
 interface InputField<TFields extends {}>
-  extends Field<'email' | 'password' | 'number'> {
-  name: Extract<keyof TFields, string>
-  validate?: Validator
+  extends Field<"email" | "password" | "number"> {
+  name: Extract<keyof TFields, string>;
+  validate?: Validator;
+  suffix?: string;
+  showErrors?: boolean;
 }
 
-interface SubmitField extends Field<'submit'> {}
+interface SubmitField extends Field<"submit"> {}
 
-export type Props<T extends {} = never> = InputField<T> | SubmitField
+export type Props<T extends {} = never> = InputField<T> | SubmitField;
 
 let FormItem = <TFields extends {}>(props: Props<TFields>) => {
-  if (props.type === 'submit') {
-    return <Submit label={props.label} />
+  if (props.type === "submit") {
+    return <Submit label={props.label} />;
   } else {
     return (
       <Input
@@ -27,9 +29,11 @@ let FormItem = <TFields extends {}>(props: Props<TFields>) => {
         name={props.name}
         type={props.type}
         validate={props.validate}
+        suffix={props.suffix}
+        showErrors={props.showErrors === undefined ? true : props.showErrors}
       />
-    )
+    );
   }
-}
+};
 
-export default FormItem
+export default FormItem;
